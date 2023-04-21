d4dfc1c4be1c8eef6ec243babe8582875f968320
SilinsLVT <lukass6lgs@gmail.com>
2023-04-21 10:11:35 +0300

    Pabeigti teksta logi, ka ari izvade no dota info

---
+++ Assets/Skripti/VardaParadisana.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VardaParadisana : MonoBehaviour {
+	private string teksts;
+	private int vecums;
+
+	public GameObject Varda_Ievade;
+	public GameObject Vecuma_Ievade;
+	public GameObject tekstaAttelosana;
+
+	void Start() {
+		Varda_Ievade.GetComponent<InputField>().contentType = InputField.ContentType.Standard;
+		Vecuma_Ievade.GetComponent<InputField>().contentType = InputField.ContentType.IntegerNumber;
+	}
+
+	public void UzglabatTekstu(){
+		teksts = Varda_Ievade.GetComponent<Text>().text;
+		vecums = int.Parse(Vecuma_Ievade.GetComponent<Text>().text);
+		string vecaVecs = "gadus vecs";
+		if (teksts.EndsWith("a")) {
+			vecaVecs = "gadus veca";
+		}
+		tekstaAttelosana.GetComponent<Text> ().text = teksts + " ir " + vecums + " " + vecaVecs + "! ";
+	}
+}
