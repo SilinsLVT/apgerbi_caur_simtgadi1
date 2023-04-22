c47c93998818224a0c9059c68469720d2635c158
SilinsLVT <lukass6lgs@gmail.com>
2023-04-22 15:33:44 +0300

    Pabeigta dropdown funkcija

---
+++ Assets/Skripti/AtteluParadisana.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class AtteluParadisana : MonoBehaviour
+{
+    public GameObject mainigaisAttels;
+    public Sprite[] atteluMasivs;
+
+    public void izkritosais(int indekss)
+    {
+        if (indekss == 0)
+
+            mainigaisAttels.GetComponent<Image>().sprite = atteluMasivs[0];
+
+        else if (indekss == 1) mainigaisAttels.GetComponent<Image>().sprite = atteluMasivs[1];
+
+    }
+}
