d9572262917ebc7888b07ff5b1e178b95e399aa0
SilinsLVT <lukass6lgs@gmail.com>
2023-04-22 20:19:54 +0300

    Pabeigtas apgerbu kategorijas

---
+++ Assets/Skripti/DrebjuParadisana.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DrebjuParadisana : MonoBehaviour
+{
+    public GameObject Krekli;
+    public GameObject Bikses;
+    public GameObject Kurpes;
+
+
+    void Start()
+    {
+        Krekli.SetActive(false);
+        Bikses.SetActive(false);
+        Kurpes.SetActive(false);
+    }
+    public void ParaditKreklus(bool show)
+    {
+        Krekli.SetActive(show);
+    }
+
+    public void ParaditBikses(bool show)
+    {
+        Bikses.SetActive(show);
+    }
+
+    public void ParaditKurpes(bool show)
+    {
+        Kurpes.SetActive(show);
+    }
+}
