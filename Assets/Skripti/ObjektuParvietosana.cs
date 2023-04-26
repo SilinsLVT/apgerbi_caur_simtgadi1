a51f9bef69917d6e31546e3c0926fcb87bd2f976
SilinsLVT <lukass6lgs@gmail.com>
2023-04-26 18:07:18 +0300

    Pabeigta objektu parvietosana, ka ari ieliktas fona skana un skana kad nospiez pogu+ iesakti abi slaideri

---
+++ Assets/Skripti/ObjektuParvietosana.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Importet lai varetu darboties ar pointer interfeicu
+using UnityEngine.EventSystems;
+public class ObjektuParvietosana : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+
+    public Canvas kanva;
+    private RectTransform transformacijasLogs;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        transformacijasLogs = GetComponent<RectTransform>();
+    }
+
+    public void OnPointerDown(PointerEventData notikums)
+    {
+
+        Debug.Log("Izdarits kreisais klikskis uz velkama objekta!");
+
+    }
+
+    public void OnBeginDrag(PointerEventData notikums)
+    {
+        Debug.Log("Uzsakta objekta vilksana!");
+    }
+
+    public void OnDrag(PointerEventData notikums)
+    {
+        Debug.Log("Notiek objekta vilksana!");
+        transformacijasLogs.anchoredPosition += notikums.delta / kanva.scaleFactor;
+    }
+
+    public void OnEndDrag(PointerEventData notikums)
+    {
+        Debug.Log("Veikta objekta vilksana");
+    }
+}
---
+++ Assets/Skripti/Skana.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Skana : MonoBehaviour
+{
+    public AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void KlilskaSkana()
+    {
+        audioSource.Play();
+    }
+}
