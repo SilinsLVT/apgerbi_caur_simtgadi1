a3f54fde4ff21a00a064e5786027523df9cd632e
SilinsLVT <lukass6lgs@gmail.com>
2023-04-26 23:56:53 +0300

    Vel nav pabeigti slaideri, nesaprotu kadel izmet kludas. vel jaizdara: 2 scroll view par cilvekiem un japabeidz slaideri

--- Assets/Skripti/AtteluParadisana.cs
+++ Assets/Skripti/AtteluParadisana.cs
@@ -7,6 +7,7 @@ public class AtteluParadisana : MonoBehaviour
 {
     public GameObject mainigaisAttels;
     public Sprite[] atteluMasivs;
+    public GameObject IzmeraSlaideris;
 
     public void izkritosais(int indekss)
     {
@@ -15,6 +16,22 @@ public class AtteluParadisana : MonoBehaviour
             mainigaisAttels.GetComponent<Image>().sprite = atteluMasivs[0];
 
         else if (indekss == 1) mainigaisAttels.GetComponent<Image>().sprite = atteluMasivs[1];
+
+    }
+
+
+    public void mainitGarumu()
+    {
+
+        float pasreizejaisIzmers = IzmeraSlaideris.GetComponent<Slider>().value;
+        mainigaisAttels.transform.localScale = new Vector2(1f * pasreizejaisIzmers, 1f * pasreizejaisIzmers);
+
+    }
+    public void mainitPlatumu()
+    {
+
+        float pasreizejaisIzmers = IzmeraSlaideris.GetComponent<Slider>().value;
+        mainigaisAttels.transform.localScale = new Vector2(1f * pasreizejaisIzmers, 1f * pasreizejaisIzmers);
 
     }
 }
